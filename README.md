baa6b873251fe99bbea0cfb4289daf6ac9511346
ElegyD <elegyd96@gmail.com>
2020-12-03 15:33:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# NVIDIA notifier
+
+Get notified when the retailer purchase link to a NVIDIA GeForce RTX Founders Edition gets updated.
+
+## Installation
+
+### Clone this repository
+
+- `git clone https://github.com/ElegyD/nvidia-notifier.git`
+
+### Install node packages
+
+- `cd nvidia-notifier/`
+- `npm install`
+- (Optional): `npm install -g pm2`
+
+### Create a .env file
+
+Create a `.env` file in the root directory for configuration.  
+> Default values are shown below
+```
+INTERVAL=60       # Inverval in seconds to fetch NVIDIA API
+LOCALE=de-de      # Locale used to fetch NVIDIA API
+RTX_3090=false    # Enable/Disable RTX 3090
+RTX_3080=false    # Enable/Disable RTX 3080
+RTX_3070=false    # Enable/Disable RTX 3070
+RTX_3060_TI=false # Enable/Disable RTX 3060 Ti
+```
+
+### Create the nodemailer transport options file
+
+Currently the only channel to get notified is by mail. For this, the `nodemailer` module is used.
+
+- Create a `nodemailer.json` file in the root directory and enter your transport options.  
+
+Example setup for Gmail:
+
+1. Create an App password [here](https://myaccount.google.com/apppasswords)
+2. Create the `nodemailer.json` file in the root directory
+    ```json
+    {
+        "service": "gmail",
+        "auth": {
+            "user": "youraddress@gmail.com",
+            "pass": "yourapppassword"
+        }
+    }
+    ```
+
+For other providers see the nodemailer SMTP configuration [here](https://nodemailer.com/smtp/).
+
+## Usage
+
+Either:  
+- Start with node:
+    - `node index.js`
+- Start with PM2 to run it in the background:
+    - `pm2 start index.js`
---
+++ index.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+
+const https = require('https');
+const nodemailer = require('nodemailer');
+const transport = require('./nodemailer.json');
+
+const interval = Math.max(1, Number(process.env.INTERVAL) || 60) * 1000
+const locale = process.env.LOCALE || 'de-de';
+const gpus = {
+    'RTX 3090': process.env.RTX_3090,
+    'RTX 3080': process.env.RTX_3080,
+    'RTX 3070': process.env.RTX_3070,
+    'RTX 3060 Ti': process.env.RTX_3060_TI
+};
+const selectedGPUs = Object.keys(gpus).filter(key => gpus[key] === 'true').join();
+
+const url = `https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=${locale}&category=GPU&gpu=${selectedGPUs}&manufacturer=NVIDIA&manufacturer_filter=NVIDIA~2,ASUS~7,EVGA~10,GAINWARD~0,GIGABYTE~6,MSI~2,PNY~4,ZOTAC~3`;
+const options = {};
+
+var purchaseLinks = new Set();
+
+// Fetch once to get current purchase links
+fetchProductDetails(products => {
+    for (productDetails of products) {
+        for (retailer of productDetails['retailers']) {
+            var purchaseLink = retailer['purchaseLink'];
+            purchaseLinks.add(purchaseLink);
+        }
+    }
+
+    setupFinished();
+});
+
+function setupFinished() {
+    setInterval(() => {
+        fetchProductDetails(products => {
+            var time = new Date().toLocaleTimeString();
+            for (productDetails of products) {
+                var productTitle = productDetails['productTitle'];
+                //var productPrice = productDetails['productPrice'];
+                var retailers = productDetails['retailers'];
+                if (retailers.length == 0) {
+                    console.log(`${time}: ${productTitle} - NO retailers`);
+                } else {
+                    for (retailer of retailers) {
+                        var purchaseLink = retailer['purchaseLink'];
+                        if (purchaseLinks.has(purchaseLink)) {
+                            console.log(`${time}: ${productTitle} - OLD purchaseLink ${purchaseLink}`);
+                        } else {
+                            console.log(`${time}: ${productTitle} - NEW purchaseLink ${purchaseLink}`);
+                            purchaseLinks.add(purchaseLink);
+                            sendMail(productTitle, purchaseLink);
+                        }
+                    }
+                }
+            }
+        });
+    }, interval);
+}
+
+function sendMail(productTitle, purchaseLink) {
+    if (!Object.keys(transport).length) {
+        return;
+    }
+
+    var transporter = nodemailer.createTransport(transport);
+
+    var mailOptions = {
+        from: transport.auth.user,
+        to: transport.auth.user,
+        subject: productTitle,
+        text: purchaseLink
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+function fetchProductDetails(callback) {
+    https.get(url, options, res => {
+        res.on('data', data => {
+            var json = JSON.parse(data);
+            var searchedProducts = json['searchedProducts'];
+            var featuredProduct = searchedProducts['featuredProduct'];
+            var productDetails = searchedProducts['productDetails'];
+            var products = [];
+            products.push(featuredProduct);
+            for (product of productDetails) {
+                products.push(product);
+            }
+            callback(products);
+        });
+    }).on('error', error => {
+        console.log(error);
+    });
+}
